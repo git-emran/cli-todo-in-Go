6a9285c7a40ed8cb87c13db19cddebc60c7c5538
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2025-10-04 10:36:08 +0600

    Table View Created

--- main.go
+++ main.go
@@ -2,9 +2,12 @@ package main
 
 func main() {
 	todos := Todos{}
+	storage := NewStorage[Todos]("todos.json")
+	storage.Load(&todos)
 	todos.add("Buy milk")
 	todos.add("Buy bread")
 	todos.toggle(0)
 	todos.print()
+	storage.Save(todos)
 
 }
